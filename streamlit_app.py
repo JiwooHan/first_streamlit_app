45573b4c3c560bbc38e51f54d3ba5897476c7e96
JiwooHan <43608137+JiwooHan@users.noreply.github.com>
2023-03-26 21:59:34 +0900

    Update streamlit_app.py
    We took 6 lines of code and re-distributed them so that we now have 1) a function that queries the table and 2) a button that calls our function and loads the data onto the page.

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+streamlit.(fruits_to_show)
 
 #create the repetable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
@@ -51,13 +51,18 @@ except URLError as e:
 streamlit.stop()
 
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#snowflake-related functions
+def get_fruit_load_list():
+   with my_cur = my_cnx.cursor():
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+
+#add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
 
 
 
